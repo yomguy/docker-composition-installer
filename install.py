0e635c2abcc19606e85fc0c6820ca018a04cf89e
Emilie Zawadzki <emilie.zawadzki@ircam.fr>
2018-11-15 16:05:08 +0100

    re add self.root placeholder on cron

--- install.py
+++ install.py
@@ -180,7 +180,7 @@ class DockerCompositionInstaller(object):
         rule = f.read()
         f.close()
         f = open('/etc/cron.d/' + self.name, 'w')
-        f.write(rule)
+        f.write(rule % self.root)
         f.close()
 
     def uninstall_daemon_sysvinit(self):
